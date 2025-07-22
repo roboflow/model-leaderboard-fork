04f404d6ecc1bcb83c449626739780b8416dba48
Chris Doss <chris@roboflow.com>
2025-07-22 10:50:26 -0400

    Add basePath and assetPrefix to next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,7 +4,8 @@ const nextConfig = {
   trailingSlash: true,
   images: {
     unoptimized: true
-  }
-}
+  },
+  basePath: '/model-leaderboard-fork',
+  assetPrefix: '/model-leaderboard-fork',
 
 module.exports = nextConfig
